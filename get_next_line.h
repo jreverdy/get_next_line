a5040306e2ef796b1a4f07c4830496f7b11f425c
jreverdy <jeremy.reverdy.zjr@gmail.com>
2021-12-14 17:19:56 +0100

    final push inchallah

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jereverd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 12:29:39 by jereverd          #+#    #+#             */
+/*   Updated: 2021/12/13 12:29:43 by jereverd         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2, size_t i, size_t j);
+size_t	ft_strlen(char *str);
+int		check_buffer(char *line);
+
+#endif
